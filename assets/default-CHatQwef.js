751f2b8d1cb1fed118159af83d094ad7a7d0ee7f
manuel <riviere.manuel@gmail.com>
2026-03-30 01:20:06 +0200

    Updates

---
+++ assets/default-CHatQwef.js
@@ -0,0 +1,12 @@
+const n=`{
+  "type": "follow",
+  "priority": 5,
+  "enabled": true,
+  "params": {
+    "targetEntityId": "",
+    "speed": 2,
+    "linear": true,
+    "angular": true
+  }
+}
+`;export{n as default};
